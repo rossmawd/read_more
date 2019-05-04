b8ab47d5a4571b0e21fb5e29563f7bc32d60961b
Lauren Carne <oreocookie@hotmail.co.uk>
2019-05-04 19:45:37 +0100

    Fixed module relasionships

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,7 @@
 class Book < ActiveRecord::Base
-    has_many :user_books 
-    has_many :user_books, through: :user_books
-    belongs_to :users
-    belongs_to :authors
-    belongs_to :genres
+    has_many :user_books
+    has_many :users, through: :user_books
+    belongs_to :user
+    belongs_to :author
+    belongs_to :genre
 end
-    
--- app/models/user_book.rb
+++ app/models/user_book.rb
@@ -1,5 +1,13 @@
 class User_Book < ActiveRecord::Base
-    belongs_to :books
-    belongs_to :users
+    belongs_to :book
+    belongs_to :user
+
+    def self.update_id
+      # Updates all user_books ID's
+      all_user_books = self.all.each do |user_books|
+        user_books.id +=10
+        user_books.save
+      end
+    end
+
 end
-    
