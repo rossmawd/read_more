f0099f3719bac33d3c170fc9373c8f3f66d5f06a
Lauren Carne <oreocookie@hotmail.co.uk>
2019-05-07 19:27:47 +0100

    Fixed Database issues.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ActiveRecord::Base
   has_many :books
   has_many :user_books
   has_many :genres, through: :books
-  has_many :authors, through: :books
   has_many :books, through: :user_books
 
   def update_password(password)
