3da72ea27aa44129d227d0da7645086e64e3fcd6
Lauren Carne <oreocookie@hotmail.co.uk>
2019-05-07 17:17:21 +0100

    Updated inner menu

--- bin/run.rb
+++ bin/run.rb
@@ -2,6 +2,10 @@ require_relative '../config/environment'
 require_relative './command_line_interface'
 
 prompt = TTY::Prompt.new
+font = TTY::Font.new(:standard)
+pastel = Pastel.new
+
+welcome
 
 puts "Hello and welcome to the book keeper!"
 
