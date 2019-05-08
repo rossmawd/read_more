e354603098d71c727a5f5afbe0333c366670bd5d
Ross Mawdsley <rossmawd@gmail.com>
2019-05-08 11:36:00 +0100

    added method to find isbn_13

--- bin/APIAccessor.rb
+++ bin/APIAccessor.rb
@@ -13,11 +13,11 @@ puts " "
 puts "Thanks! Searching for book..."
 puts " "               #is there a better way for entering blank lines??
 
- result = RestClient.get("https://www.googleapis.com/books/v1/volumes?q=#{answer}")
+result = RestClient.get("https://www.googleapis.com/books/v1/volumes?q=#{answer}")
 
- book_search_results = JSON.parse(result)
+book_search_results = JSON.parse(result)
 
- puts "Books found!"
+puts "Books found!"
 
  
  # Displays the title, author, publishedDate for the top 3? results
@@ -41,51 +41,48 @@ puts " "               #is there a better way for entering blank lines??
   choice = gets.chomp
   choice = choice.to_i   #Will break if they don't give a number (1,2,3 etc) 
 
+  #HELPER:not every book has 'authors'
   def check_if_authors_key_exists(index, book_search_results)
     authors = book_search_results["items"][index]["volumeInfo"]["authors"]
     authors == nil ? output = nil : output = authors.join(", ")
     output
   end
 
-
-
-def add_new_book_from_api(choice, user_id, book_search_results)
-  index = choice - 1
-  #authors = book_search_results["items"][index]["volumeInfo"]["authors"]
-  #authors == nil ? output = nil : output = authors.join(", ")
-  check_if_authors_key_exists(index, book_search_results)
-  #Below searhes ALL books, need to specify only the user's books (Once I move this into the User class):
- if Book.all.find_by(isbn_13: book_search_results["items"][index]["volumeInfo"]["industryIdentifiers"][0]["identifier"]) == nil
-    Book.create(
-      name: book_search_results["items"][index]["volumeInfo"]["title"], 
-      synopsis: book_search_results["items"][index]["volumeInfo"]["description"], 
-      user_id: user_id, 
-      author: check_if_authors_key_exists(index, book_search_results),
-      isbn_13: book_search_results["items"][index]["volumeInfo"]["industryIdentifiers"][0]["identifier"] #the API is inconsistant here! 
-      #sometimes this array position is ISBN10, sometimes ISBN13
-      ##h.include?(key) --> true/false
-      )
-  else
-    puts "You already own this book! Please select a new one!" #NEED TO LOOP!
+  #HELPER find's if the book has ISBN_13, else nil
+  def find_isbn_13(index, book_search_results)
+    length = book_search_results["items"][index]["volumeInfo"]["industryIdentifiers"].length
+    i = 0
+    isbn = 0
+    length.times {
+      isbn_hash = book_search_results["items"][index]["volumeInfo"]["industryIdentifiers"][i]
+      if isbn_hash.value?("ISBN_13")
+        isbn = isbn_hash["identifier"]
+        break
+      else
+        isbn = nil
+      end
+      i +=1
+      isbn
+    }  
+    isbn
   end
-end
+
+  def add_new_book_from_api(choice, user_id, book_search_results)
+    index = choice - 1
+
+    #Below searhes ALL books, need to specify only the user's books (Once I move this into the User class):
+  if Book.all.find_by(isbn_13: book_search_results["items"][index]["volumeInfo"]["industryIdentifiers"][0]["identifier"]) == nil
+      Book.create(
+        name: book_search_results["items"][index]["volumeInfo"]["title"], 
+        synopsis: book_search_results["items"][index]["volumeInfo"]["description"], 
+        user_id: user_id, 
+        author: check_if_authors_key_exists(index, book_search_results), 
+        isbn_13: find_isbn_13(index, book_search_results)
+        )
+    else
+      puts "You already own this book! Please select a new one!" #NEED TO LOOP!
+    end
+  end
 
 add_new_book_from_api(choice, 3, book_search_results)
 
-
-
- #  book["volumeInfo"].each do |volInfo|
-
- #   binding.pry
-      #title = RestClient.get(volInfo)
-      #title_hash = JSON.parse(title)
-      #binding.pry
-      #puts title_hash["title"]
-  #end
-
-
-#  book["films"].each do |film_url|
-#     film_res = RestClient.get(film_url)
-#     film_hash = JSON.parse(film_res)
-#     puts film_hash["title"]
-# end
