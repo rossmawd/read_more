3f3a0df8e0d4c520dbb32a7fa2edbc56736d6dff
Lauren Carne <oreocookie@hotmail.co.uk>
2019-05-09 18:29:07 +0100

    Added easter egg

--- bin/run.rb
+++ bin/run.rb
@@ -5,13 +5,14 @@ prompt = TTY::Prompt.new
 font = TTY::Font.new(:standard)
 pastel = Pastel.new
 
-Cli.welcome
-Cli.bookcase
+# Cli.welcome
+# Cli.bookcase
+#
+# Cli.line
+# sleep 0.5
+# Cli.quotation
+# sleep 0.5
+# Cli.line
+# Cli.start_menu
 
-Cli.line
-sleep 0.5
-Cli.quotation
-sleep 0.5
-Cli.line
-# puts "Lets get started: "
-Cli.start_menu
+Cli.easter_egg
