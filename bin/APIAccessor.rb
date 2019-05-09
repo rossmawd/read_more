c01462b5c1f4849b444150b3b058b2e3105522e6
Ross Mawdsley <rossmawd@gmail.com>
2019-05-09 12:10:14 +0100

    connected all APIAccess. methods so called from 1

--- bin/APIAccessor.rb
+++ bin/APIAccessor.rb
@@ -3,29 +3,43 @@ require 'rest-client'
 require 'json'
 require_relative '../config/environment'   #Ross added this
 
+  #HELPER:not every book has 'authors'
+  def check_if_authors_key_exists(index, api_result)
+    authors = api_result["items"][index]["volumeInfo"]["authors"]
+    authors == nil ? output = "No Author found" : output = authors.join(", ")
+    output
+  end
+
+  #HELPER
+  def check_if_genre_key_exists(index, api_result)
+    genres = api_result["items"][index]["volumeInfo"]["categories"]
+    genres == nil ? output = "No Genre found" : output = genres.join(", ")
+    output
+  end
+
+  #HELPER find's if the book has ISBN_13, else nil
+  def find_isbn_13(index, api_result)
+    length = api_result["items"][index]["volumeInfo"]["industryIdentifiers"].length
+    i = 0
+    isbn = 0
+    length.times {
+      isbn_hash = api_result["items"][index]["volumeInfo"]["industryIdentifiers"][i]
+      if isbn_hash.value?("ISBN_13")
+        isbn = isbn_hash["identifier"]
+        break
+      else
+        isbn = nil
+      end
+      i +=1
+      isbn
+    }  
+    isbn
+  end
+
 #####BELOW: the CLI for Entering A book using the API
 
-def get_input_and_search_api
-  puts "Welcome to Google Books API "
-  puts "Please enter a book title 📚"
-  answer = gets.chomp
-
-  puts 
-  puts "Thanks! Searching for book... 🔍"
-  puts               #is there a better way for entering blank lines??
-
-  result = RestClient.get("https://www.googleapis.com/books/v1/volumes?q=#{answer}")
-
-  book_search_results = JSON.parse(result)
-
-  puts "Books found! 😀  Here are the top 3:"
-  book_search_results
-end
-
-#api_result = get_input_and_search_api
- 
  # Displays the title, author, publishedDate for the top 3? results  
-def display_three_books(i = 0, api_result)
+ def display_three_books(i = 0, api_result)
   3.times do
       puts "Book #{i+1}"
       puts "Title: #{api_result["items"][i]["volumeInfo"]["title"]}\n"
@@ -40,10 +54,7 @@ def display_three_books(i = 0, api_result)
   end
 end
 
-#display_three_books(0, api_result) 
-
-
-def book_choice_menu(api_result, i)
+def book_choice_menu(api_result)
   menu = TTY::Prompt.new
  
   selection = menu.select("What would you like to do?") do |a|
@@ -52,7 +63,6 @@ def book_choice_menu(api_result, i)
     a.choice ''
     a.choice '❌  Exit'
   end
-
   i = 3
   case selection
     when '📚  See the next 3 books'
@@ -76,69 +86,72 @@ def book_choice_menu(api_result, i)
   end
 end
 
-#book_choice_menu(api_result, i)
+#The user then chooses the one they want to enter into their database:
+def book_choice
+  puts "Please enter a book\'s number to enter it into your library: "
+  choice = gets.chomp
+  choice = choice.to_i   #Will break if they don't give a number (1,2,3 etc) 
+end
 
+#Need to connect to a user (Once I move this into the User class):
+def add_new_book_from_api(book_choice, user_id, api_result)
+  index = book_choice - 1
 
- #The user then chooses the one they want to enter into their database:
-  def book_choice
-    puts "Please enter a book\'s number to enter it into your library: "
-    choice = gets.chomp
-    choice = choice.to_i   #Will break if they don't give a number (1,2,3 etc) 
+  if Book.all.find_by(isbn_13: api_result["items"][index]["volumeInfo"]["industryIdentifiers"][0]["identifier"]) == nil
+    Book.create(
+      name: api_result["items"][index]["volumeInfo"]["title"], 
+      synopsis: api_result["items"][index]["volumeInfo"]["description"],
+      genre: check_if_genre_key_exists(index, api_result),
+      user_id: user_id, 
+      author: check_if_authors_key_exists(index, api_result), 
+      isbn_13: find_isbn_13(index, api_result)
+      )
+      puts "'#{api_result["items"][index]["volumeInfo"]["title"]}' has been saved to your Library!" 
+  else
+    puts "You already own this book! Please select a new one!" #NEED TO LOOP!
   end
+end
 
-  #HELPER:not every book has 'authors'
-  def check_if_authors_key_exists(index, api_result)
-    authors = api_result["items"][index]["volumeInfo"]["authors"]
-    authors == nil ? output = nil : output = authors.join(", ")
-    output
-  end
+#This method collects the user's search input and calls all other methods needed to add book via API
+def get_input_and_search_api
+  puts "Welcome to Google Books API "
+  puts "Please enter a book title or author name (or both!) 📚"
+  answer = gets.chomp
 
-  #HELPER find's if the book has ISBN_13, else nil
-  def find_isbn_13(index, api_result)
-    length = api_result["items"][index]["volumeInfo"]["industryIdentifiers"].length
-    i = 0
-    isbn = 0
-    length.times {
-      isbn_hash = api_result["items"][index]["volumeInfo"]["industryIdentifiers"][i]
-      if isbn_hash.value?("ISBN_13")
-        isbn = isbn_hash["identifier"]
-        break
-      else
-        isbn = nil
-      end
-      i +=1
-      isbn
-    }  
-    isbn
-  end
+  puts 
+  puts "Thanks! Searching for book... 🔍"
+  puts               #is there a better way for entering blank lines??
 
-  #Need to connect to a user (Once I move this into the User class):
-  def add_new_book_from_api(book_choice, user_id, api_result)
-    index = book_choice - 1
+  result = RestClient.get("https://www.googleapis.com/books/v1/volumes?q=#{answer}")
 
-    if Book.all.find_by(isbn_13: api_result["items"][index]["volumeInfo"]["industryIdentifiers"][0]["identifier"]) == nil
-      Book.create(
-        name: api_result["items"][index]["volumeInfo"]["title"], 
-        synopsis: api_result["items"][index]["volumeInfo"]["description"],
-        genre: api_result["items"][index]["volumeInfo"]["categories"].join(", "),
-        user_id: user_id, 
-        author: check_if_authors_key_exists(index, api_result), 
-        isbn_13: find_isbn_13(index, api_result)
-        )
-        puts "'#{api_result["items"][index]["volumeInfo"]["title"]}' has been saved to your Library!" 
-    else
-      puts "You already own this book! Please select a new one!" #NEED TO LOOP!
-    end
-  end
+  puts "Books found! 😀  Here are the top 3:"
+  book_search_results = JSON.parse(result)
+
+  display_three_books(i=0, book_search_results)
+  book_choice_menu(book_search_results)
+  add_new_book_from_api(book_choice,3, book_search_results)
+  #########add_new_book_menu
+
+end
+
+get_input_and_search_api
+
+
+
  
 
-api_result = get_input_and_search_api #returns the API hash
 
-display_three_books(0, api_result) #iterates through above HASH's attributes and puts them out
+  
+ 
 
-book_choice_menu(api_result, i=0) #asks the user if they want to see more books
-###### HERE!!
+
+
+#display_three_books(0, api_result) #iterates through above HASH's attributes and puts them out
+
+#book_choice_menu(api_result, i=0) #asks the user if they want to see more books
+
 #book_choice #Promts the user to make a numerical choice
 
-add_new_book_from_api(book_choice, 3, api_result)
+#add_new_book_from_api(book_choice, 3, api_result)
+
 
